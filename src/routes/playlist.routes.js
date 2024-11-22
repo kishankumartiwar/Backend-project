0f7391da9bbacd67454904eac81c8f80ee2279d2
kishankumartiwar <tiwarikishan297@gmail.com>
2024-11-23 00:00:50 +0530

    removed comment code in playlist

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,4 +1,4 @@
-/* import { Router } from 'express';
+import { Router } from 'express';
 import {
     addVideoToPlaylist,
     createPlaylist,
@@ -27,4 +27,4 @@ router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
 
 router.route("/user/:userId").get(getUserPlaylists);
 
-export default router */
+export default router
